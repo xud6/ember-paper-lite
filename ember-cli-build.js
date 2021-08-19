6bf98a44bfa934cafbebfd0cd75aeb06f39b0795
徐岱青 <m@xud6.com>
2021-08-19 09:05:18 +0800

    v3.20.2...v3.24.0

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,7 +1,7 @@
 'use strict';
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
     snippetPaths: ['tests/dummy/snippets'],
